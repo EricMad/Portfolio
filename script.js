0b91221cc349d67ddbf2f1b09e91810990e1d351
EricMad <ermadou@gmail.com>
2023-05-02 14:39:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const textAnim = document.querySelector(".presentation__informations__h1");
+
+new Typewriter(textAnim, {
+  deleteSpeed: 20,
+})
+  .changeDelay(40)
+  .typeString("Eric Madou")
+  .pauseFor(300)
+  .typeString(', Developpeur <span style="color:#d33bfd">web</span>')
+  .pauseFor(1000)
+  .deleteChars(3)
+  .pauseFor(1000)
+  .typeString('<span style="color:#E44D26" > HTML</span>')
+  .pauseFor(1000)
+  .deleteChars(4)
+  .pauseFor(1000)
+  .typeString('<span style="color:#2965f1"> CSS</span>')
+  .pauseFor(1000)
+  .deleteChars(3)
+  .pauseFor(1000)
+  .typeString('<span style= "color:#F0DB4F"> JavaScript</span>')
+  .pauseFor(1000)
+  .deleteChars(10)
+  .pauseFor(1000)
+  .typeString('<span style="color:#61dafb"> ReactJS</span>')
+  .pauseFor(1000)
+  .deleteChars(7)
+  .pauseFor(1000)
+  .typeString('<span style="color:#d33bfd">web.</span>')
+
+  .start();
+
+const btn = document.querySelector(".btn-rtn");
+
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
